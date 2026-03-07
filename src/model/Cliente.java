4dda353e17c1ae1352dc084bf5f0676c808572ff
c.pineda <c.pineda@unillanos.edu.co>
2026-03-07 12:46:08 -0500

    Enhance Cliente class with input validation
    Added validation to setNombre and setTelefono methods.

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,5 @@
 package model;
+import service.IdGenerator;
 
 public class Cliente {
     private int id;
@@ -11,8 +12,20 @@ public class Cliente {
         this.telefono = telefono;
     }
 
-    public void setNombre(String nombre) { this.nombre = nombre; }
     public int getId() { return this.id; }
     public String getNombre() { return this.nombre; }
     public String getTelefono() { return this.telefono; }
+    public void setTelefono(String telefono) {
+        if (telefono == null || telefono.isBlank()) return;
+        this.telefono = telefono;
+    }
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isBlank()) return;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente ID = " + id + ", Nombre ='" + nombre + "'" + ", Tel = " + telefono;
+    }
 }
